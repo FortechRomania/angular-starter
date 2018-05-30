65e263e9691015df6e2afe0b8f98927123805651
Helga Gabriela Paulger <helga.bucur@fortech.ro>
2018-05-30 18:23:39 +0300

    Add user class and users mock data

---
+++ src/app/mock-users.ts
@@ -0,0 +1,12 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    { id: 1, name: 'Jon Doe' },
+    { id: 2, name: 'Jane Doe'},
+    { id: 3, name: 'Mark Clarck'},
+    { id: 4, name: 'Joe Shmoe'},
+    { id: 5, name: 'Louie Booey'},
+    { id: 6, name: 'Alfred Malfred'},
+    { id: 7, name: 'Rick Rickson'},
+    { id: 8, name: 'Harry Berry'},
+];
