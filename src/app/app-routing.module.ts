18034891ce6937cd5ee76a99d20cd7b40f7a20af
Helga Gabriela Paulger <helga.bucur@fortech.ro>
2018-05-31 17:13:48 +0300

    Add dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UsersComponent } from './users/users.component';
+
 
 const appRoutes: Routes = [
     { path: '', component: HomepageComponent },
+    { path: 'dashboard', component: DashboardComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'users', component: UsersComponent}
+    { path: 'users', component: UsersComponent},
+    { path: 'user/:id', component: UserDetailComponent}
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+
+import { UserService } from './user.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    DashboardComponent,
     HomepageComponent,
+    LoginComponent,
     RegisterComponent,
-    UsersComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,7 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
